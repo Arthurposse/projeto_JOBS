15005e2b416e25a931f3274f664fb35be83c12b1
unknown <apossebonn@gmail.com>
2024-04-26 20:31:51 -0300

    atualizando_JOBS_26.04

---
+++ CodigosProjeto/Front/Tela Cadastro/Tela Cadastro - Usuario Empresa/main.js
@@ -0,0 +1,31 @@
+let button = document.getElementById('handleSubmit');
+
+button.onclick = async function (e) {
+    e.preventDefault();
+    //cancela o comportamento padrão de um formulario, tem que colocar o "e" no parametro
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let data = { name, email, password }
+
+    // POST
+    const response = await fetch('http://localhost:3008/api/store/task', {
+        method: "POST",
+        headers: { "Content-type": "application/json;charset=UTF-8" },
+        body: JSON.stringify(data)
+    });
+
+    let content = await response.json();
+    console.log(content);
+
+    if (content.sucess) {
+        alert ("Sucesso com o POST!!");
+        // window.location.reload();
+        //recarrega a página
+
+    } else {
+        console.error()
+        alert("Não deu o POST!!");
+    };
+};
--- CodigosProjeto/Front/Tela Home - Sem Usuario Logado/main.js
+++ CodigosProjeto/Front/Tela Home - Sem Usuario Logado/main.js
@@ -1,11 +1,11 @@
-setTimeout(() => {
-    Swal.fire({
-        title: "Não realizou seu cadastro?",
-        text: "Clique abaixo:",
-        icon: "question",
-        html: `
-        <a href="../Tela Cadastro/Tela Cadastro - Verificando Tipo Usuario/index.html" style="text-decoration: none; color: #19a7ce;"> Cadastrar-se </a>`,
-        confirmButtonColor: "#0e566a",
-        confirmButtonText: "Fazer mais tarde"
-    });
-}, 4000);
+// setTimeout(() => {
+//     Swal.fire({
+//         title: "Não realizou seu cadastro?",
+//         text: "Clique abaixo:",
+//         icon: "question",
+//         html: `
+//         <a href="../Tela Cadastro/Tela Cadastro - Verificando Tipo Usuario/index.html" style="text-decoration: none; color: #19a7ce;"> Cadastrar-se </a>`,
+//         confirmButtonColor: "#0e566a",
+//         confirmButtonText: "Fazer mais tarde"
+//     });
+// }, 4000);
