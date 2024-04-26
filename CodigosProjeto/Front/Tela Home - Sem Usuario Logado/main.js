dde41416aa06429887f080823fd7b24b1645c857
unknown <apossebonn@gmail.com>
2024-04-25 23:31:54 -0300

    atualizando_JOBS_25.04

---
+++ CodigosProjeto/Front/Tela Home - Sem Usuario Logado/main.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    Swal.fire({
+        title: "Não realizou seu cadastro?",
+        text: "Clique abaixo:",
+        icon: "question",
+        html: `
+        <a href="../Tela Cadastro/Tela Cadastro - Verificando Tipo Usuario/index.html" style="text-decoration: none; color: #19a7ce;"> Cadastrar-se </a>`,
+        confirmButtonColor: "#0e566a",
+        confirmButtonText: "Fazer mais tarde"
+    });
+}, 4000);
