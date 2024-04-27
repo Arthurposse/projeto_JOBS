47c000e4a3409b4a1ba08240623579f25dd1ec07
unknown <apossebonn@gmail.com>
2024-04-26 22:27:26 -0300

    atualizando_JOBS_26.04

---
+++ CodigosProjeto/Front/Tela Cadastro/Tela Cadastro - Usuario Jovem/main.js
@@ -0,0 +1,34 @@
+let button = document.querySelector('button');
+
+button.onclick = async function (e) {
+    e.preventDefault();
+    //cancela o comportamento padrão de um formulario, tem que colocar o "e" no parametro
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let data_nascimento = document.getElementById('data_nascimento').value;
+    let telefone = document.getElementById('telefone').value;
+    let cidade = document.getElementById('cidade').value;
+    let data = { name, email, password, data_nascimento, telefone, cidade }
+
+    // POST
+    const response = await fetch('http://localhost:3008/api/user/jovem', {
+        method: "POST",
+        headers: { "Content-type": "application/json;charset=UTF-8" },
+        body: JSON.stringify(data)
+    });
+
+    let content = await response.json();
+    console.log(content);
+    
+    if (content.sucess) {
+        alert ("Sucesso com o POST!!");
+        // window.location.reload();
+        //recarrega a página
+
+    } else {
+        console.error()
+        alert("Não deu o POST!!");
+    };
+};
