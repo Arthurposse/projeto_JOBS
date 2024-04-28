7845a3c19a29c73f53d1dd171f19dc84dbdd4b9e
unknown <apossebonn@gmail.com>
2024-04-28 19:06:59 -0300

    atualizando_JOBS_28.04

---
+++ CodigosProjeto/Front/Tela LogIn/Tela Login - Entrando/main.js
@@ -0,0 +1,55 @@
+let button = document.querySelector('button');
+
+button.onclick = async function (e) {
+    e.preventDefault();
+    //cancela o comportamento padrão de um formulario, tem que colocar o "e" no parametro
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    // POST
+    const response = await fetch('http://localhost:3008/api/verif/logIn', {
+        method: "GET",
+        headers: { "Content-type": "application/json;charset=UTF-8" }
+    });
+
+    let content = await response.json();
+    console.log(content);
+    
+    if (content.sucess) {
+        alert ("Sucesso com o GET!!");
+
+        // Iterando sobre o array de chaves
+        for (let i = 0; i < content.data.length; i++) {
+            
+            if(content.data[i].email === email) {
+                Swal.fire({
+                    title: "LogIn realizado com sucesso!!",
+                    icon: "success",
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+                setTimeout(() => {
+                    window.location.href = '../../Tela Home - Sem Usuario Logado/index.html';
+                }, 2000);
+                break
+            }
+            
+            else {
+                Swal.fire({
+                    title: "Conta não encontrada!!",
+                    icon: "error",
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+            }
+        }
+
+        // window.location.reload();
+        //recarrega a página
+
+    } else {
+        console.error()
+        alert("Não deu o GET!!");
+    };
+};
