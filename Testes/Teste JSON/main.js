f1b1e296ad1064abc008fc99de5ae4a6ef4fa6fd
Arthur <aspossebonn@gmail.com>
2024-06-07 11:00:18 -0300

    atualizando_JOBS_07.06

---
+++ Testes/Teste JSON/main.js
@@ -0,0 +1,23 @@
+const teste_josn = [
+    {
+        "pergunta": "",
+        "alternativas": ["A) TESTE 1", "B) TESTE 2", "C) TESTE 3"],
+        "explicação": "BLA BLA"
+    },
+
+    {
+        "pergunta": "",
+        "alternativas": ["A) TESTE 1", "B) TESTE 2", "C) TESTE 3"],
+        "explicação": "BLA BLA"
+    },
+
+    {
+        "pergunta": "",
+        "alternativas": ["A) TESTE 1", "B) TESTE 2", "C) TESTE 3"],
+        "explicação": "BLA BLA"
+    }
+]
+
+teste_josn.forEach((e) => {
+    console.log(e)
+})
