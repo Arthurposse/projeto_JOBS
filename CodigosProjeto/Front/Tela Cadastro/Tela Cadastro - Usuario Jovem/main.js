69f12757269344d54b181327593fb2be5d33dc8e
unknown <apossebonn@gmail.com>
2024-06-21 00:46:22 -0300

    atualizando_JOBS_21.06

--- CodigosProjeto/Front/Tela Cadastro/Tela Cadastro - Usuario Jovem/main.js
+++ CodigosProjeto/Front/Tela Cadastro/Tela Cadastro - Usuario Jovem/main.js
@@ -7,66 +7,78 @@ button.onclick = async function (e) {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
+  let confirm_password = document.getElementById("confirm_password").value;
   let data_nascimento = document.getElementById("data_nascimento").value;
   let telefone = document.getElementById("telefone").value;
   let cidade = document.getElementById("cidade").value;
 
   let data = { name, email, password, data_nascimento, telefone, cidade };
 
-  // POST
-  const response = await fetch("http://localhost:3008/api/user/jovem", {
-    method: "POST",
-    headers: { "Content-type": "application/json;charset=UTF-8" },
-    body: JSON.stringify(data),
-  });
-
-  let content = await response.json();
-  console.log(content);
-
-  if (content.sucess) {
-    Swal.fire({
-      title: "Cadastro realizado com sucesso!!",
-      text: "Aguarde enquanto seu código de verificação é gerado.",
-      icon: "success",
-      showConfirmButton: false,
-      timer: 2600,
-    });
-
-    const response = await fetch("http://localhost:3008/api/testeEnviarEmail", {
+  if (password === confirm_password) {
+    // POST
+    const response = await fetch("http://localhost:3008/api/user/jovem", {
       method: "POST",
       headers: { "Content-type": "application/json;charset=UTF-8" },
-      body: JSON.stringify({ email }),
+      body: JSON.stringify(data),
     });
 
     let content = await response.json();
     console.log(content);
 
-    if (content.success) {
+    if (content.sucess) {
       Swal.fire({
-        title: "Código de verificação enviado com sucesso!!",
-        text: "Acesse seu email.",
+        title: "Cadastro realizado com sucesso!!",
+        text: "Aguarde enquanto seu código de verificação é gerado.",
         icon: "success",
         showConfirmButton: false,
-        timer: 2100,
+        timer: 2600,
       });
 
-      localStorage.setItem("CodVerif", content.verificationCode);
+      const response = await fetch("http://localhost:3008/api/testeEnviarEmail", {
+        method: "POST",
+        headers: { "Content-type": "application/json;charset=UTF-8" },
+        body: JSON.stringify({ email }),
+      });
 
-      setTimeout(() => {
-        window.location.href =
-        "../../Tela Cadastro/Tela Cadastro - Verificando Codigo/index.html";
-      }, 2100);
+      let content = await response.json();
+      console.log(content);
+
+      if (content.success) {
+        Swal.fire({
+          title: "Código de verificação enviado com sucesso!!",
+          text: "Acesse seu email.",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 2100,
+        });
+
+        localStorage.setItem("CodVerif", content.verificationCode);
+
+        setTimeout(() => {
+          window.location.href =
+            "../../Tela Cadastro/Tela Cadastro - Verificando Codigo/index.html";
+        }, 2100);
+      } else {
+        Swal.fire({
+          title: "Conta não encontrada!!",
+          text: "Tente novamente!! Ou crie uma conta!!",
+          icon: "error",
+          showConfirmButton: false,
+          timer: 2300,
+        });
+      }
     } else {
-      Swal.fire({
-        title: "Conta não encontrada!!",
-        text: "Tente novamente!! Ou crie uma conta!!",
-        icon: "error",
-        showConfirmButton: false,
-        timer: 2300,
-      });
+      console.error();
+      alert("Não deu o POST!!");
     }
-  } else {
-    console.error();
-    alert("Não deu o POST!!");
+  }
+  else {
+    Swal.fire({
+      title: "As senhas não coincidem!!",
+      text: "Tente novamente!!",
+      icon: "error",
+      showConfirmButton: false,
+      timer: 2300,
+    });
   }
 };
