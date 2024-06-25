2eaed576220e85bd3805d185874e814139e25f01
Arthur Possebon <apossebonn@gmail.com>
2024-06-25 09:40:19 -0300

    atualizando_JOBS_25.06

--- Testes/Tela Login-Cadastro/Teste 2 - Cadastro/main.js
+++ Testes/Tela Login-Cadastro/Teste 2 - Cadastro/main.js
@@ -1,3 +1,4 @@
+let input_senha = document.getElementById('input_senha');
 let icon_olho = document.getElementById('icon_olho');
 
 icon_olho.onclick = function(){
@@ -8,3 +9,29 @@ icon_olho.onclick = function(){
         icon_olho.className = "bi bi-eye-slash-fill";
     }
 }
+
+// // Selecionar todos os elementos com a classe 'icon-olho'
+// var iconOlhos = document.querySelectorAll('.icon-olho');
+
+// // Iterar sobre cada ícone de olho
+// iconOlhos.forEach(function(iconOlho) {
+//     iconOlho.onclick = function() {
+//         var inputSenha = iconOlho.previousElementSibling; // Selecionar o input anterior ao ícone de olho
+
+//         if (iconOlho.classList.contains('bi-eye-slash-fill')) {
+//             iconOlho.classList.remove('bi-eye-slash-fill');
+//             iconOlho.classList.add('bi-eye-fill');
+//             inputSenha.type = 'text';
+//         } else {
+//             iconOlho.classList.remove('bi-eye-fill');
+//             iconOlho.classList.add('bi-eye-slash-fill');
+            
+//             var novoInput = document.createElement('input');
+//             novoInput.type = 'password';
+//             novoInput.value = inputSenha.value;
+//             novoInput.placeholder = inputSenha.placeholder;
+
+//             inputSenha.parentNode.replaceChild(novoInput, inputSenha);
+//         }
+//     };
+// });
