1216d95a2509a57861bafe5a9e235cfafcc28e39
Arthur Possebon <apossebonn@gmail.com>
2024-07-06 11:50:40 -0300

    atualizando_JOBS_06.07

--- CodigosProjeto/Back/src/app.js
+++ CodigosProjeto/Back/src/app.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
 
-const router = require('./routes/MainRouter');
+const routerJovem = require('./routes/UserJovemRouter');
+const routerEmpresa = require('./routes/UserEmpresaRouter');
 const enviarEmailRouter = require('./routes/enviarEmailRouter');
-
 
 dotenv.config();
 
@@ -13,7 +13,8 @@ app.set('port', process.env.PORT || 3008);
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', router);
+app.use('/api', routerJovem);
+app.use('/api', routerEmpresa);
 app.use('/api', enviarEmailRouter);
 
 module.exports = app;
