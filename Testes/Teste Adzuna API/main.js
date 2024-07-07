f0ed1a3fec3f5a45b63158ee4f911c2186694d64
Arthur de Souza Possebon <arthur.possebon@ska.com.br>
2024-07-07 20:09:37 -0300

    atualizando_JOBS_07.07

--- Testes/Teste Adzuna API/main.js
+++ Testes/Teste Adzuna API/main.js
@@ -31,6 +31,10 @@ botao.onclick = function(e){
     })
     .then((data) => {
       console.log("Resposta da API Adzuna: ", data);
+
+      if(data.results.length === 0) {
+        alert('Não foi encontrado nenhuma vaga!')
+      }
   
       const jobsListElement = document.querySelector("section");
   
@@ -49,17 +53,23 @@ botao.onclick = function(e){
   
         const locationElement = document.createElement("p");
         locationElement.textContent = `Localização: ${data.results[i].location.display_name}`;
+
+        const dataElement = document.createElement("p");
+        let data_criacao = data.results[i].created;
+        dataElement.textContent = `Data de criação da vaga: ${data_criacao.slice(8, 10)}/${data_criacao.slice(5, 7)}/${data_criacao.slice(0, 4)} as ${data_criacao.slice(11, 16)}`
   
         const linkElement = document.createElement('a');
         linkElement.href = data.results[i].redirect_url; // Suponha que redirect_url seja o link detalhado da vaga
         linkElement.textContent = "Detalhes da Vaga"; // Texto do link
+        linkElement.target = "_blank"; // Abrir em nova aba
   
         jobElement.appendChild(titleElement);
         jobElement.appendChild(descriptionElement);
         jobElement.appendChild(companyElement);
         jobElement.appendChild(locationElement);
+        jobElement.appendChild(dataElement);
         jobElement.appendChild(linkElement);
-  
+        
         jobsListElement.appendChild(jobElement);
       }
     })
