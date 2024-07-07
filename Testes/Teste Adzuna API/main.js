d637492101dbfdeb52483f6e1d8b5782b6b9015c
Arthur de Souza Possebon <arthur.possebon@ska.com.br>
2024-07-07 19:41:34 -0300

    atualizando_JOBS_07.07

--- Testes/Teste Adzuna API/main.js
+++ Testes/Teste Adzuna API/main.js
@@ -1,61 +1,69 @@
 // Exemplo de como enviar uma solicitação para o servidor
 const url = "http://localhost:3000/api/jobs";
 
-const payload = {
-  // Parâmetros de busca como keywords, location, etc.
-  keywords: "Empacotador",
-  location: "",
-};
+const botao = document.querySelector('button');
 
-const headers = {
-  "Content-Type": "application/json",
-};
+botao.onclick = function(e){
+  e.preventDefault();
 
-fetch(url, {
-  method: "POST",
-  headers: headers,
-  body: JSON.stringify(payload),
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`Erro ao fazer a solicitação: ${response.status}`);
-    }
-    return response.json();
+  const input = document.querySelector('input').value;
+
+  const payload = {
+    // Parâmetros de busca como keywords, location, etc.
+    keywords: input,
+    location: "",
+  };
+  
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  
+  fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(payload),
   })
-  .then((data) => {
-    console.log("Resposta da API Adzuna: ", data);
-
-    const jobsListElement = document.querySelector("section");
-
-    for (let i = 0; i < data.results.length; i++) {
-      const jobElement = document.createElement("div");
-      jobElement.classList.add("job-item");
-
-      const titleElement = document.createElement("h3");
-      titleElement.textContent = `Título Vaga: ${data.results[i].title}`;
-
-      const descriptionElement = document.createElement("p");
-      descriptionElement.textContent = `Descrição: ${data.results[i].description}`;
-
-      const companyElement = document.createElement("p");
-      companyElement.textContent = `Empresa: ${data.results[i].company.display_name}`;
-
-      const locationElement = document.createElement("p");
-      locationElement.textContent = `Localização: ${data.results[i].location.display_name}`;
-
-      const linkElement = document.createElement('a');
-      linkElement.href = data.results[i].redirect_url; // Suponha que redirect_url seja o link detalhado da vaga
-      linkElement.textContent = "Detalhes da Vaga"; // Texto do link
-
-      jobElement.appendChild(titleElement);
-      jobElement.appendChild(descriptionElement);
-      jobElement.appendChild(companyElement);
-      jobElement.appendChild(locationElement);
-      jobElement.appendChild(linkElement);
-
-      jobsListElement.appendChild(jobElement);
-    }
-  })
-  .catch((error) => {
-    console.error("Erro:", error);
-  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Erro ao fazer a solicitação: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Resposta da API Adzuna: ", data);
+  
+      const jobsListElement = document.querySelector("section");
+  
+      for (let i = 0; i < data.results.length; i++) {
+        const jobElement = document.createElement("div");
+        jobElement.classList.add("job-item");
+  
+        const titleElement = document.createElement("h3");
+        titleElement.textContent = `Título Vaga: ${data.results[i].title}`;
+  
+        const descriptionElement = document.createElement("p");
+        descriptionElement.textContent = `Descrição: ${data.results[i].description}`;
+  
+        const companyElement = document.createElement("p");
+        companyElement.textContent = `Empresa: ${data.results[i].company.display_name}`;
+  
+        const locationElement = document.createElement("p");
+        locationElement.textContent = `Localização: ${data.results[i].location.display_name}`;
+  
+        const linkElement = document.createElement('a');
+        linkElement.href = data.results[i].redirect_url; // Suponha que redirect_url seja o link detalhado da vaga
+        linkElement.textContent = "Detalhes da Vaga"; // Texto do link
+  
+        jobElement.appendChild(titleElement);
+        jobElement.appendChild(descriptionElement);
+        jobElement.appendChild(companyElement);
+        jobElement.appendChild(locationElement);
+        jobElement.appendChild(linkElement);
+  
+        jobsListElement.appendChild(jobElement);
+      }
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+    });
+}
