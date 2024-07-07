b94233f453110b116d2a772abf9bdada9b309aea
Arthur de Souza Possebon <arthur.possebon@ska.com.br>
2024-07-07 19:05:36 -0300

    atualizando_JOBS_07.07

--- Testes/Teste Adzuna API/main.js
+++ Testes/Teste Adzuna API/main.js
@@ -4,11 +4,11 @@ const url = "http://localhost:3000/api/jobs";
 const payload = {
   // Parâmetros de busca como keywords, location, etc.
   keywords: "Desenvolvedor de software",
-  location: ""
+  location: "",
 };
 
 const headers = {
-  "Content-Type": "application/json"
+  "Content-Type": "application/json",
 };
 
 fetch(url, {
@@ -16,23 +16,23 @@ fetch(url, {
   headers: headers,
   body: JSON.stringify(payload),
 })
-.then(response => {
-  if (!response.ok) {
-    throw new Error(`Erro ao fazer a solicitação: ${response.status}`);
-  }
-  return response.json();
-})
-.then(data => {
-  console.log("Resposta da API Adzuna: ", data);
-  
-  for(let i = 0; i < data.results.length; i++) {
-    console.log(`Título Vaga: ${data.results[i].title}`)
-    console.log(`Descrição: ${data.results[i].description}`)
-    console.log(`Empresa: ${data.results[i].company.display_name}`)
-    console.log(`Localização: ${data.results[i].location.display_name}`)
-    console.log("");
-  }
-})
-.catch(error => {
-  console.error("Erro:", error);
-});
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Erro ao fazer a solicitação: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Resposta da API Adzuna: ", data);
+
+    for (let i = 0; i < data.results.length; i++) {
+      console.log(`Título Vaga: ${data.results[i].title}`);
+      console.log(`Descrição: ${data.results[i].description}`);
+      console.log(`Empresa: ${data.results[i].company.display_name}`);
+      console.log(`Localização: ${data.results[i].location.display_name}`);
+      console.log("");
+    }
+  })
+  .catch((error) => {
+    console.error("Erro:", error);
+  });
--- Testes/Teste Adzuna API/server.js
+++ Testes/Teste Adzuna API/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import fetch from "node-fetch";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,14 +9,16 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/jobs', async (req, res) => {
-  const apiUrl = "https://api.adzuna.com/v1/api/jobs/br/search/1";  // Endpoint para buscar vagas no Brasil
-  const appId = "";  // Seu app_id da Adzuna
-  const appKey = "";  // Sua app_key da Adzuna
+app.post("/api/jobs", async (req, res) => {
+  const apiUrl = "https://api.adzuna.com/v1/api/jobs/br/search/1"; // Endpoint para buscar vagas no Brasil
+  const appId = "2f83b376"; // Seu app_id da Adzuna
+  const appKey = "9388be172961eea6d70a24fc4bbcd0b2"; // Sua app_key da Adzuna
   const { keywords, location } = req.body;
 
   try {
-    const url = `${apiUrl}?app_id=${appId}&app_key=${appKey}&what=${encodeURIComponent(keywords)}&where=${encodeURIComponent(location)}`;
+    const url = `${apiUrl}?app_id=${appId}&app_key=${appKey}&what=${encodeURIComponent(
+      keywords
+    )}&where=${encodeURIComponent(location)}`;
     const response = await fetch(url);
 
     if (!response.ok) {
