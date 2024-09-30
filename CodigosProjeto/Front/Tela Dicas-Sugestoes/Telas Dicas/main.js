55af6144c32af18a183f96342f9ab2e01c9cd1c2
ARTHUR DE SOUZA POSSEBON <03819771026@senacrs.edu.br>
2024-09-30 11:55:45 -0300

    atualizando_JOBS_30.09

--- CodigosProjeto/Front/Tela Dicas-Sugestoes/Telas Dicas/main.js
+++ CodigosProjeto/Front/Tela Dicas-Sugestoes/Telas Dicas/main.js
@@ -2,17 +2,19 @@
 
 let tema_escolhido = localStorage.getItem("tema_escolhido");
 
-if(tema_escolhido === 'Currículo') {
+let titulo_header = document.getElementById("titulo_header");
 
+if(tema_escolhido === 'Currículo') {
+    titulo_header.textContent = "Como desenvolver um currículo ideial?";
 }
 else if(tema_escolhido === 'Entrevista') {
-
+    titulo_header.textContent = "Como sair bem em uma entrevista?";
 }
 else if(tema_escolhido === 'Planejamento de carreira') {
-
+    titulo_header.textContent = "";
 }
 else if(tema_escolhido === 'Escolhendo sua área') {
-
+    titulo_header.textContent = "Qual área é melhor escolher para minha vida?";
 }
 else {
     alert("ERROR! Não foi escolhida a dica!!");
