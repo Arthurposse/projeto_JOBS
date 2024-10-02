f8da12226bb2f35d6364a04df2557c9bfcc8040e
Arthur Possebon <apossebonn@gmail.com>
2024-10-01 22:21:50 -0300

    atualizando_JOBS_01.10

--- CodigosProjeto/Front/Tela Dicas-Sugestoes/Telas Dicas/main.js
+++ CodigosProjeto/Front/Tela Dicas-Sugestoes/Telas Dicas/main.js
@@ -11,18 +11,23 @@ user_logado.textContent = User_name;
 let tema_escolhido = localStorage.getItem("tema_escolhido");
 
 let titulo_header = document.getElementById("titulo_header");
+let titulo_main = document.getElementById("titulo_main");
 
 if(tema_escolhido == 'Currículo') {
     titulo_header.textContent = "Como desenvolver um currículo ideial?";
+    titulo_main.textContent = "Currículo";
 }
 else if(tema_escolhido == 'Entrevista') {
     titulo_header.textContent = "Como sair bem em uma entrevista?";
+    titulo_main.textContent = "Entrevista";
 }
 else if(tema_escolhido == 'Planejamento de carreira') {
     titulo_header.textContent = "";
+    titulo_main.textContent = "Planejamento de Carreira";
 }
 else if(tema_escolhido == 'Escolhendo sua área') {
     titulo_header.textContent = "Qual área é melhor escolher para minha vida?";
+    titulo_main.textContent = "Escolhendo sua Área";
 }
 else {
     alert("ERROR! Não foi escolhida a dica!!");
