ac92d40e36643ccd868f36aecfb955addce231a9
Arthur Possebon <apossebonn@gmail.com>
2024-11-06 20:52:27 -0300

    atualizando_JOBS_06.11

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -44,8 +44,8 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, '..', 'Frontend')));
 
 // Rota para acessar o 'index.html' da pasta 'Tela Home - Sem Usuario Logado'
-app.get('/teste', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'Frontend', 'Tela Home - Sem Usuario Logado', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Frontend', 'Tela Home - Sem Usuario Logado', 'index.html'));
 });
 
 // Rotas da API
