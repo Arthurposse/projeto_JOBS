e85cc277b963047dd2fd357cbfab3efeeecaee4b
Arthur Possebon <apossebonn@gmail.com>
2024-11-06 21:25:48 -0300

    atualizando_JOBS_06.11

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -33,13 +33,12 @@ const swaggerOptions = {
 
 dotenv.config();
 
-// Serve arquivos estáticos da pasta 'Frontend'
-app.use(express.static(path.join(__dirname, '..', 'Frontend')));
+app.use(express.static(path.join(__dirname, '..', '..', 'Frontend')));
 
-// Exemplo de rota para servir o arquivo index.html da pasta 'Tela Home - Sem Usuario Logado'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/Tela Home - Sem Usuario Logado/index.html"));
-});
+    res.sendFile(path.join(__dirname, '..', '..', 'Frontend', 'Tela Home - Sem Usuario Logado', 'index.html'));
+  });
+  
 
 app.set("port", process.env.PORT || 3010);
 app.use(cors());
